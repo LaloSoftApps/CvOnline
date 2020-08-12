33d3deac8264c166f2ce1a2240a39f147749d5b6
LaloSoft <gsantilli@gmail.com>
2020-08-12 17:43:38 -0300

    Uso de imagenes locales, segmentadas por dni en parametro id de la URL

--- CvWeb/Controllers/HomeController.cs
+++ CvWeb/Controllers/HomeController.cs
@@ -11,12 +11,19 @@ namespace CvWeb.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string id)
         {
+            if (id == null)
+            {
+                id = "28308948";
+            }
+            ViewData["id"] = id;
+
             // Obtenemos los datos del JSON
             var oJson = new JsonFileRead();
-            oJson.FilePath = Environment.CurrentDirectory + "/Data/cv_28308948.json";
-            
+            // oJson.FilePath = Environment.CurrentDirectory + "/Data/cv_28308948.json";
+            oJson.FilePath = $"{Environment.CurrentDirectory}/wwwroot/data/{id}/cv.json";
+
             try
             {
                 var JsonData = oJson.Call();
